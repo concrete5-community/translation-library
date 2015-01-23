cc4f93a468d97ebc9e820edf02f0136ccc16563c
Michele Locati <michele@locati.it>
2015-01-23 10:12:28 +0100

    Improve ThemePresets parser

--- src/Parser/ThemePresets.php
+++ src/Parser/ThemePresets.php
@@ -29,55 +29,49 @@ class ThemePresets extends \C5TL\Parser
     {
         $themesPresets = array();
         $prefix = ($relativePath === '') ? '' : "$relativePath/";
-        foreach ($this->getDirectoryStructure($rootDirectory) as $child) {
-            $shownChild = $prefix . $child;
-            if (preg_match('%(?:^|/)themes/\w+/css/presets$%', $shownChild, $matches)) {
-                $presetsAbsDirectory = "$rootDirectory/$child";
-                $hDir = @opendir($presetsAbsDirectory);
-                if ($hDir === false) {
-                    throw new \Exception("Unable to open directory $presetsAbsDirectory");
+        foreach (array_merge(array(''), $this->getDirectoryStructure($rootDirectory)) as $child) {
+            $presetsAbsDirectory = ($child === '') ? $rootDirectory : "$rootDirectory/$child";
+            if (preg_match('%(?:^|/)themes/\w+/css/presets$%', $presetsAbsDirectory, $matches)) {
+                $dirList = @scandir($presetsAbsDirectory);
+                if ($dirList === false) {
+                    throw new \Exception("Unable to parse directory $presetsAbsDirectory");
                 }
-                try {
-                    while (($file = @readdir($hDir)) !== false) {
-                        if (($file[0] !== '.') && preg_match('/[^.].*\.less$/i', $file)) {
-                            $fileAbs = "$presetsAbsDirectory/$file";
-                            if (is_file($fileAbs)) {
-                                $content = @file_get_contents($fileAbs);
-                                if ($content === false) {
-                                    throw new \Exception("Error reading file '$fileAbs'");
-                                }
-                                $content = str_replace("\r", "\n", str_replace("\r\n", "\n", $content));
-                                // Strip multiline comments
-                                $content = preg_replace_callback(
-                                    '|/\*.*?\*/|s',
-                                    function ($matches) {
-                                        return str_repeat("\n", substr_count($matches[0], "\n"));
-                                    },
-                                    $content
-                                );
-                                foreach (array("'", '"') as $quote) {
-                                    if (preg_match('%(?:^|\\n|;)[ \\t]*@preset-name:\\s*' . $quote . '([^' . $quote . ']*)' . $quote . '\\s*(?:;|$)%s', $content, $matches)) {
-                                        $presetName = $matches[1];
-                                        $presetLine = null;
-                                        $p = strpos($content, $matches[0]);
-                                        if ($p !== false) {
-                                            $presetLine = substr_count(substr($content, 0, $p), "\n") + 1;
-                                        }
-                                        if (!isset($themesPresets[$presetName])) {
-                                            $themesPresets[$presetName] = array();
-                                        }
-                                        $themesPresets[$presetName][] = array($shownChild . "/$file", $presetLine);
-                                        break;
+                $shownChild = ($child === '') ? rtrim($prefix, '/') : ($prefix . $child);
+                foreach($dirList as $file) {
+                    if (($file[0] !== '.') && preg_match('/[^.].*\.less$/i', $file)) {
+                        $fileAbs = "$presetsAbsDirectory/$file";
+                        if (is_file($fileAbs)) {
+                            $content = @file_get_contents($fileAbs);
+                            if ($content === false) {
+                                throw new \Exception("Error reading file '$fileAbs'");
+                            }
+                            $content = str_replace("\r", "\n", str_replace("\r\n", "\n", $content));
+                            // Strip multiline comments
+                            $content = preg_replace_callback(
+                                '|/\*.*?\*/|s',
+                                function ($matches) {
+                                    return str_repeat("\n", substr_count($matches[0], "\n"));
+                                },
+                                $content
+                            );
+                            foreach (array("'", '"') as $quote) {
+                                if (preg_match('%(?:^|\\n|;)[ \\t]*@preset-name:\\s*' . $quote . '([^' . $quote . ']*)' . $quote . '\\s*(?:;|$)%s', $content, $matches)) {
+                                    $presetName = $matches[1];
+                                    $presetLine = null;
+                                    $p = strpos($content, $matches[0]);
+                                    if ($p !== false) {
+                                        $presetLine = substr_count(substr($content, 0, $p), "\n") + 1;
                                     }
+                                    if (!isset($themesPresets[$presetName])) {
+                                        $themesPresets[$presetName] = array();
+                                    }
+                                    $themesPresets[$presetName][] = array($shownChild . "/$file", $presetLine);
+                                    break;
                                 }
                             }
                         }
                     }
-                } catch (\Exception $x) {
-                    @closedir($hDir);
-                    throw $x;
                 }
-                @closedir($hDir);
             }
         }
         foreach ($themesPresets as $themesPreset => $references) {
