200340dfd2eb4478a8471f55334e95ea7043e338
Michele Locati <michele@locati.it>
2015-04-13 14:35:31 +0200

    Add support for Trees and Topics strings

---
+++ src/Parser/DynamicItem/Tree.php
@@ -0,0 +1,24 @@
+<?php
+namespace C5TL\Parser\DynamicItem;
+
+/**
+ * Extract translatable data from AttributeSets
+ */
+class Tree extends DynamicItem
+{
+    /**
+     * @see \C5TL\Parser\DynamicItem::getParsedItemNames()
+     */
+    public function getParsedItemNames()
+    {
+        return function_exists('t') ? t('Trees and Topics') : 'Trees and Topics';
+    }
+
+    /**
+     * @see \C5TL\Parser\DynamicItem::getClassNameForExtractor()
+     */
+    protected function getClassNameForExtractor()
+    {
+        return '\Concrete\Core\Tree\Tree';
+    }
+}
