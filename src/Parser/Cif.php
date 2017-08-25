43aa3806c36b095d458fe394feea353013f745dd
Michele Locati <michele@locati.it>
2017-08-25 07:44:27 +0200

    Add support to geolocation libraries

--- src/Parser/Cif.php
+++ src/Parser/Cif.php
@@ -128,6 +128,7 @@ class Cif extends \C5TL\Parser
             case '/concrete5-cif/gatheringitemtemplates':
             case '/concrete5-cif/gatheringitemtemplates/gatheringitemtemplate/feature':
             case '/concrete5-cif/gatheringsources':
+            case '/concrete5-cif/geolocators':
             case '/concrete5-cif/imageeditor_components':
             case '/concrete5-cif/imageeditor_controlsets':
             case '/concrete5-cif/imageeditor_filters':
@@ -219,6 +220,7 @@ class Cif extends \C5TL\Parser
             case '/concrete5-cif/features':
             case '/concrete5-cif/flag_types':
             case '/concrete5-cif/gatheringitemtemplatetypes':
+            case '/concrete5-cif/geolocators/geolocator/option':
             case '/concrete5-cif/pages/page/area/block/arealayout/columns/column/block/data/record':
             case '/concrete5-cif/pages/page/area/blocks':
             case '/concrete5-cif/pages/page/area/style':
@@ -378,6 +380,10 @@ class Cif extends \C5TL\Parser
                 break;
             case '/concrete5-cif/pagetypes/pagetype/composer/formlayout/set/control':
                 static::readXmlNodeAttribute($translations, $filenameRel, $node, 'custom-label', 'PageTypeComposerFormLayoutSetControlCustomLabel');
+                break;
+            case '/concrete5-cif/geolocators/geolocator':
+                static::readXmlNodeAttribute($translations, $filenameRel, $node, 'name', 'GeolocatorName');
+                static::readXmlNodeAttribute($translations, $filenameRel, $node, 'description', 'GeolocatorDescription');
                 break;
             default:
                 if (strpos($filenameRel, 'packages/') === 0) {
